34817cabccef058b4983775c31ed59781453dd6f
dwaynek24 <devinwayneknotts@gmail.com>
2022-10-21 10:16:22 -0700

    oct 21 1016am

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Content from './components/Content';
 import Nav from './components/Nav';
 import SubjectSearch from './components/SubjectSearch';
 import Form from './components/Form';
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link, NavLink} from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
 import { useState } from "react";
 import CreateForm from './CreateForm';
+import Home from './components/Home';
 
 function App() {
   const [page, setPage] = useState('');
   const navigate= useNavigate();
 
   function getActivity() {
-    const url = "http://localhost:3000/";
+    const url = "https://zacs-book-e-api.herokuapp.com/";
 
     fetch(url)
       .then((response) => response.json())
@@ -31,23 +32,28 @@ function App() {
   return (
     
     <>
-    <nav>
+    
     <div className="App">
-      
 
+      
+      <Nav>
       <Link to='/'>
       <Header />
       </Link>
-      
+      <Link to='createsheet'>
+      <h1>createsheet</h1>
+      </Link>
+      </Nav>
       </div>
-    </nav>
+    
   
       <Nav className="App"/>
-      <CreateForm />
+      
       <Content />
       <SubjectSearch handleClick={handleClick}/>
       <Routes>
-        <Route path='/' element={<Header />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='createsheet' element={<CreateForm/>} />
       </Routes>
       
       </>
