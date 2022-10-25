99c23fa062367fcab9543a4524072fe9f7a11a71
mariesaoconnell <mariesa.oconnell@gmail.com>
2022-10-25 13:13:38 -0400

    added styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,24 @@
 
 function Home() {
 	return (
-		<nav className='home'>
-			<h4>👋 Welcome to DocuSet 👋 </h4>
-			<p>
-				Whether you're creating new notes, updating and searching for old notes, or doing some
-				docu-spring-deleting, DocuSet is THE place for your sheets to live!{' '}
-			<br />
-				Checkout our <em>In Development</em> tab to see what our AMAZING dev team has cookin' up! 🍳
-			</p>
-
+		<div className='home'>
+			<h2 className="home-header">👋 Welcome to DocuSet 👋 </h2>
 			<div className='homeImage'>
 				<img
 					src='https://cdn.pixabay.com/photo/2020/04/30/10/15/cute-notes-5112237_1280.png'
 					alt=''
 				/>
 			</div>
-		</nav>
+			<div className="home-text">
+				<p>
+					Whether you're creating new notes, updating and searching for old notes, or doing some
+					docu-spring-deleting, DocuSet is THE place for your sheets to live!{' '}
+				<br />
+					Checkout our <em>GitHub</em> tab to see what our AMAZING dev team has cookin' up! 🍳
+				</p>
+			</div>
+
+		</div>
 	);
 }
 
