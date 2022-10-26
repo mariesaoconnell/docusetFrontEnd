00e1b682ed39d6d6c2e782bd4f793c85313e5663
mariesaoconnell <mariesa.oconnell@gmail.com>
2022-10-26 15:31:07 -0400

    change title and favicon

--- src/components/nav components/Nav.js
+++ src/components/nav components/Nav.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 function Nav(props) {
 	return (
 		<div className='navDiv'>
-			<Link to="/"><h3>Home</h3></Link>
+			<Link to="/">Home</Link>
 			<Link to='/MySheets'>
-				<h3>My Sheets</h3>
+				My Sheets
 			</Link>
 			<Link to='/create'>
-				<h3>Create Sheet</h3>
+				Create Sheet
 			</Link>
 		</div>
 	);
